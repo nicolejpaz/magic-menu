6dd214e1d31591f51d4ad29c9071416bdb549ade
Sherif Abushadi <sherif@amgando.org>
2013-11-16 11:10:51 -0800

    wip. first pass at routes

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,57 @@
 get '/' do
   # Look in app/views/index.erb
-  erb :index
+  # erb :index
+  redirect '/menus'
 end
+
+
+#######################################
+# MENUS
+#######################################
+# decided to change this from the spec
+#  because it's a better route
+get '/menus' do
+	@menus = Menu.all
+	erb :menus
+end
+
+# add a new menu
+post '/menus' do
+	# expecting params[:menu] as new menu fields
+	Menu.create(params[:menu])
+	redirect '/menus'
+end
+
+
+#######################################
+# ITEMS
+#######################################
+get '/items' do
+	@items = Item.all
+	erb :items
+end
+
+# add a new menu
+post '/items' do
+	# expecting params[:menu] as new menu fields
+	Item.create(params[:item])
+	redirect '/items'
+end
+
+
+#######################################
+# MENUS and ITEMS
+#######################################
+get '/menus/:id' do
+	@menu = Menu.find(params[:id])
+	@items = Item.all
+	erb :menu
+end
+
+post '/menus/:id' do
+	@menu = Menu.find(params[:id])
+	@item = Item.create(params[:item])
+	@menu.items.add(@item)
+	redirect "/menus/#{params[:id]}"
+end
+
